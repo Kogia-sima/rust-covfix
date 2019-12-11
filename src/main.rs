a2fd77fe5d9fa96592c7bd58452f8a424fe18182
Kogia-sima <orcinus4627@gmail.com>
2019-12-11 16:35:02 +0900

    feat: new command line option: '--root'

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,25 @@ use std::process;
 use cobertura::CoberturaParser;
 use common::{CoverageReader, CoverageWriter};
 
+#[cfg(not(windows))]
+const COV_DIR: &str = "target/cov";
+
+#[cfg(windows)]
+const COV_DIR: &str = "target\\cov";
+
 fn main() {
-    process_args();
-    let file = search_coverage_file();
+    let options = process_args();
+
+    let cov_dir = if let Some(mut root) = options.root {
+        root.push(COV_DIR);
+        if !root.exists() {
+            panic!("Run `cargo kcov` before execution.");
+        }
+        root
+    } else {
+        find_cov_dir()
+    };
+    let file = search_coverage_file(cov_dir);
 
     let cf = CoberturaParser::new();
     let mut coverage = cf.load_coverages(&file);
@@ -19,23 +35,41 @@ fn main() {
     cf.save_coverages(&file, &coverage);
 }
 
-fn process_args() {
+struct Arguments {
+    root: Option<PathBuf>
+}
+
+fn process_args() -> Arguments {
+    let mut results = Arguments { root: None };
     let mut args = env::args();
     args.next().unwrap();
 
-    for arg in args {
+    while let Some(arg) = args.next() {
         match &*arg {
             "-h" | "--help" => show_usage(),
             "-v" | "--version" => {
                 println!("v{}", env!("CARGO_PKG_VERSION"));
                 process::exit(0);
-            }
+            },
+            "--root" => {
+                if let Some(arg) = args.next() {
+                    if !arg.starts_with("-") {
+                        results.root = Some(PathBuf::from(arg));
+                        continue;
+                    }
+                }
+
+                eprintln!("--root option requires an argument.");
+                show_usage();
+            },
             invalid_arg => {
                 eprintln!("invalid argument: \"{}\"\n", invalid_arg);
                 show_usage();
             }
         }
     }
+
+    results
 }
 
 fn show_usage() {
@@ -47,7 +81,8 @@ fn show_usage() {
             "  kcov-coverage-fix [OPTIONS]\n\n",
             "Options:\n",
             "  -h --help:    show usage\n",
-            "  -v --version: output version information",
+            "  -v --version: output version information\n",
+            "     --root:    specify project root directory"
         ),
         env!("CARGO_PKG_VERSION"),
         env!("CARGO_PKG_AUTHORS")
@@ -56,32 +91,32 @@ fn show_usage() {
     process::exit(1);
 }
 
-fn search_coverage_file() -> PathBuf {
-    let mut p = find_target_dir();
-    p.push("cov");
+fn search_coverage_file(mut p: PathBuf) -> PathBuf {
     p.push("kcov-merged");
     p.push("cobertura.xml");
 
     p
 }
 
-fn find_target_dir() -> PathBuf {
+fn find_cov_dir() -> PathBuf {
     let mut path = env::current_dir().expect("cannot detect the current directory.");
-    path.push("target");
+    path.push(COV_DIR);
     
     if path.is_dir() {
         return path;
     }
 
     path.pop();
+    path.pop();
 
     while path.pop() {
-        path.push("target");
+        path.push(COV_DIR);
 
         if path.is_dir() {
             return path;
         }
 
+        path.pop();
         path.pop();
     }
 
